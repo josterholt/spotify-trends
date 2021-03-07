8d8bd0f8c7185102847eb5f9c9ce0a05ad49e630
Justin Osterholt <shroder@gmail.com>
2021-03-07 08:27:04 -0800

    Setting up caching

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,24 @@ import ReactDOM from "react-dom";
 import { createHook, Provider } from "overmind-react";
 import { createOvermind } from 'overmind';
 import Homepage from './pages/homepage';
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { persistQueryClient } from 'react-query/persistQueryClient-experimental'
+import { createLocalStoragePersistor } from 'react-query/createLocalStoragePersistor-experimental'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+          cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+        },
+      },
+});
+
+const localStoragePersistor = createLocalStoragePersistor()
+persistQueryClient({
+    queryClient,
+    persistor: localStoragePersistor,
+  })
 const config = {
     state: {
         "title": "My App"
@@ -14,4 +31,9 @@ const overmind = createOvermind(config, {
     devtools: true
 });
 
-ReactDOM.render(<Provider value={overmind}><Homepage /></Provider>, document.getElementById('app'));
+ReactDOM.render(<Provider value={overmind}>
+        <QueryClientProvider client={queryClient}>
+            <Homepage />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    </Provider>, document.getElementById('app'));
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { useQuery, useQueryClient  } from 'react-query'
 import { DataGrid, GridRowsProp, GridColDef, GridCellParams } from "@material-ui/data-grid";
 
 // Get the hash of the url
@@ -13,53 +14,51 @@ const hash = window.location.hash
     return initial;
 }, {});
 
+var SpotifyWebApi = require('spotify-web-api-node');    
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+var scopes = ['user-top-read'],
+redirectUri = 'http://localhost:8080',
+clientId = 'b244422e8a674d8fa6d67b4e3eda3f2d',
+state = '1',
+showDialog = true,
+responseType = 'token';
 
 const homepage = function () {
-    const authEndpoint = 'https://accounts.spotify.com/authorize';
-    var scopes = ['user-top-read'],
-    redirectUri = 'http://localhost:8080',
-    clientId = 'b244422e8a674d8fa6d67b4e3eda3f2d',
-    state = '1',
-    showDialog = true,
-    responseType = 'token';
-  
-    // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-    var SpotifyWebApi = require('spotify-web-api-node');    
+
+    const [spotifyTimeRange, setSpotifyTimeRange] = useState("short_term")
+
     var spotifyApi = new SpotifyWebApi({
         redirectUri: redirectUri,
         clientId: clientId
     });
     
-    let logged_in = false;
-    const [listInitialized, setListInitialized] = useState(false);
-    const [trackList, setTrackList] = useState([]);
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
-
-    let track_list:any = <div></div>;
-    useEffect(() => {
-        if(!listInitialized && "access_token" in hash) {
-            setIsLoggedIn(true);
+    const queryClient = useQueryClient()
+    const { isLoading, isFetching, isFetchingAfterMount, isStale, isFetched, error, data }:any = useQuery('trackData-' + spotifyTimeRange, () => new Promise((resolve, reject) => {
+        if(!isFetched && "access_token" in hash) {
             spotifyApi.setAccessToken(hash.access_token);
-            
-            /* Get a User’s Top Artists*/
-            spotifyApi.getMyTopTracks({limit: 50, time_range: "short_term"})
+            spotifyApi.getMyTopTracks({limit: 50, time_range: spotifyTimeRange})
             .then(function(data:any) {
-                setTrackList(data.body.items)
-                setListInitialized(true);
+                resolve(data.body.items)
             }, function(err:any) {
-                setIsLoggedIn(false);
-                setListInitialized(true);
-    
-                console.log('Something went wrong!', err);
-            });
-    
+                reject()
+            })
         }
-    })
+    }),
+    {
+        staleTime: 1000 * 60 * 60 * 24, // 24 hours
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+        initialData: () => {
+            return queryClient.getQueryData('trackData')
+          },
+    });
+
 
     let columns:GridColDef[] = [
         {
             field: 'album_cover', 
-            headerName: '', 
+            headerName: ' ', 
             width: 150,
             type: "string",
             renderCell: (params: GridCellParams) => (<><img src={params.value.toString()} width={150} /></>)
@@ -69,28 +68,41 @@ const homepage = function () {
         { field: 'artist', headerName: 'Artist', width: 300 }
     ];
 
-    let rows:GridRowsProp  = trackList.map(function (track:any) {
-        return { 
-            id: track.id,
-            title: track.name,
-            album: track.album.name,
-            album_cover: track.album.images[1].url,
-            artist: track.artists.map(function (artist:any) {
-                return artist.name;
-            }).join(", ")
-        }
-    });
+    let rows:GridRowsProp = [];
+    if(data) {
+        rows = data.map(function (track:any) {
+            return { 
+                id: track.id,
+                title: track.name,
+                album: track.album.name,
+                album_cover: track.album.images[1].url,
+                artist: track.artists.map(function (artist:any) {
+                    return artist.name;
+                }).join(", ")
+            }
+        });
+    }
     
-    let loginLink = null
-    if(!isLoggedIn) {
-        loginLink = <a className="btn btn--loginApp-link"
-        href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}>
-        Login to Spotify
-        </a>
+    function handleTimeRangeChange(el:React.ChangeEvent<HTMLSelectElement>) {
+        setSpotifyTimeRange(el.target.value);
     }
 
+    
+    let loginLink = <a className="btn btn--loginApp-link"
+    href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}>
+    Login to Spotify
+    </a>
+
     return <div>
-        <h1>Demo React Application</h1>
+        <h1>Top Listened Tracks</h1>
+        <div style={{ padding: "10px"}}>
+            <label htmlFor={"time_range"}>Time Range:</label>
+            <select name="time_range" onChange={ (el) => handleTimeRangeChange(el) }>
+                <option value="short_term">Last 4 Weeks</option>
+                <option value="medium_term">Last 6 Months</option>
+                <option value="long_term">Last Several Years</option>
+            </select>
+        </div>
         <DataGrid rowHeight={150} rows={rows} columns={columns} />
         {loginLink}
     </div>
